ea9ba40f3fa3da6299ca9afbe72ad4e0636a429e
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-10-22 21:01:48 +0200

    more tests

--- tests/src/from_table.rs
+++ tests/src/from_table.rs
@@ -5,6 +5,38 @@ async fn you_shall_pass() {
   crate::models::TypesId::check_consistency().await;
 }
 
+mod altered_casing {
+  use super::*;
+
+  #[diesel_enum(conn = crate::sqlite_testing_callback, case = "PascalCase", name_mapping(default), id_mapping(default))]
+  #[allow(non_camel_case_types)]
+  enum Types {
+    grass,
+    poison,
+    fire,
+    flying,
+    water,
+    bug,
+    normal,
+    electric,
+    ground,
+    fairy,
+    fighting,
+    psychic,
+    rock,
+    steel,
+    ice,
+    ghost,
+    dragon,
+    dark,
+  }
+
+  #[tokio::test]
+  async fn altered_casing() {
+    Types::check_consistency().await;
+  }
+}
+
 mod wrong_casing {
   use super::*;
 
@@ -40,7 +72,7 @@ mod wrong_casing {
 mod name_mismatch {
   use super::*;
 
-  #[diesel_enum(conn = crate::sqlite_testing_callback, name_mapping(default), id_mapping(skip))]
+  #[diesel_enum(conn = crate::sqlite_testing_callback, case = "PascalCase", name_mapping(default), id_mapping(skip))]
   enum Types {
     #[db_mapping(name = "abc")]
     Grass,
@@ -73,7 +105,7 @@ mod name_mismatch {
 mod id_mismatch {
   use super::*;
 
-  #[diesel_enum(conn = crate::sqlite_testing_callback, name_mapping(default), id_mapping(default))]
+  #[diesel_enum(conn = crate::sqlite_testing_callback, case = "PascalCase", name_mapping(default), id_mapping(default))]
   enum Types {
     #[db_mapping(id = 20)]
     Grass,
@@ -100,5 +132,68 @@ mod id_mismatch {
   #[should_panic]
   async fn id_mismatch() {
     Types::check_consistency().await;
+  }
+}
+
+mod ignored_id_mismatch {
+  use super::*;
+
+  #[diesel_enum(conn = crate::sqlite_testing_callback, case = "PascalCase", name_mapping(default), id_mapping(skip))]
+  enum Types {
+    // Wrong order here
+    Poison,
+    Grass,
+    Fire,
+    Flying,
+    Water,
+    Bug,
+    Normal,
+    Electric,
+    Ground,
+    Fairy,
+    Fighting,
+    Psychic,
+    Rock,
+    Steel,
+    Ice,
+    Ghost,
+    Dragon,
+    Dark,
+  }
+
+  #[tokio::test]
+  async fn ignored_id_mismatch() {
+    Types::check_consistency().await;
+  }
+}
+
+mod custom_table_name {
+  use super::*;
+
+  #[diesel_enum(conn = crate::sqlite_testing_callback, case = "PascalCase", table_name = "types", name_mapping(default), id_mapping(default))]
+  enum PokeTypes {
+    Grass,
+    Poison,
+    Fire,
+    Flying,
+    Water,
+    Bug,
+    Normal,
+    Electric,
+    Ground,
+    Fairy,
+    Fighting,
+    Psychic,
+    Rock,
+    Steel,
+    Ice,
+    Ghost,
+    Dragon,
+    Dark,
+  }
+
+  #[tokio::test]
+  async fn custom_table_name() {
+    PokeTypes::check_consistency().await;
   }
 }
