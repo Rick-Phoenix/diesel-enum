8e05d2416df7ada8f4141432b74b497f91ce295a
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-10-22 14:31:43 +0200

    refactor: renamed features for consistency with macro attributes

--- src/features.rs
+++ src/features.rs
@@ -2,10 +2,14 @@ pub fn default_skip_check() -> bool {
   cfg!(feature = "default-skip-check")
 }
 
-pub fn default_text_impl() -> bool {
-  cfg!(feature = "default-text-impl")
+pub fn default_name_mapping() -> bool {
+  cfg!(feature = "default-name-mapping")
 }
 
-pub fn no_default_int_impl() -> bool {
-  cfg!(feature = "no-default-int-impl")
+pub fn no_default_id_mapping() -> bool {
+  cfg!(feature = "no-default-id-mapping")
+}
+
+pub fn async_tests() -> bool {
+  cfg!(feature = "async-tests")
 }
